6922333a2d7bc9d77f12722b4c35e5cd1ae5104a
MaiSalameh <145279461+MaiSalameh@users.noreply.github.com>
2023-12-18 14:47:25 +0200

    Update ConfTest.java

--- src/test/java/today/edu/ConfTest.java
+++ src/test/java/today/edu/ConfTest.java
@@ -1,19 +1,1 @@
-package today.edu;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.CucumberOptions.SnippetType;
-
-@RunWith(Cucumber.class)
-
-@CucumberOptions(features = "features",
-        plugin = { "summary", "html:target/cucumber"},
-        monochrome=true,
-        snippets = SnippetType.CAMELCASE,
-        glue = "today.edu")
-
-
-public class ConfTest {
-
-}
