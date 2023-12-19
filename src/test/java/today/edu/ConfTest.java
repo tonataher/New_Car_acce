1ed827b3612fcb3545a7757bd8a35aef0084cb13
MaiSalameh <145279461+MaiSalameh@users.noreply.github.com>
2023-12-19 20:44:17 +0200

    Update ConfTest.java

--- src/test/java/today/edu/ConfTest.java
+++ src/test/java/today/edu/ConfTest.java
@@ -1,1 +1,11 @@
+package today.edu;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue ="today.edu" )
+public class ConfTest {
+    User u1= new User("haya","123","7\3\2004");
+}
