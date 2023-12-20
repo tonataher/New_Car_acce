5642817df6bbe79d71331969ed7b31271eb49a18
MaiSalameh <145279461+MaiSalameh@users.noreply.github.com>
2023-12-20 19:38:45 +0200

    Update loginstep.java

--- src/test/java/today/edu/loginstep.java
+++ src/test/java/today/edu/loginstep.java
@@ -1,25 +1,17 @@
 package today.edu;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import javax.swing.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import today.edu.MyAppT;
-import today.edu.User;
 
-import javax.swing.*;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 public class loginstep {
     public MyAppT obj;
     public boolean forget = false;
-
     public String enteredUsername;
     public String enteredPassword;
-    // Dependency injection
+
     public loginstep(MyAppT iobj) {
         super();
         this.obj = iobj;
@@ -32,31 +24,23 @@ public class loginstep {
     @Given("I am not in system")
     public void mmm()
     {
-        //  obj.isLogged=false;
         obj.iAmNotInSystem(obj);
     }
     @When("set username {string} and pass {string}")
     public void setUsernameAndPassAndPassFromSystem(String user_name, String pass) {
-        // Write code here that turns the phrase above into concrete actions
-        // throw new io.cucumber.java.PendingException();
         obj.setUsernameAndPassAndPassFromSystem(user_name,pass);
     }
     @Then("login succeed")
     public void loginSucceed() {
-        // Write code here that turns the phrase above into concrete actions
-        // throw new io.cucumber.java.PendingException();
-        assertTrue("Login should succeed", obj.f);
+        assertTrue("Login should succeed", obj.validation);
     }
     @When("set invalid username {string} and pass {string}")
     public void setInvalidUsernameAndPass(String user_name, String pass) {
-        // Write code here that turns the phrase above into concrete actions
         obj.setInvalidUsernameAndPass(user_name,pass);
     }
     @Then("login failed")
     public void loginFailed() {
-        // Write code here that turns the phrase above into concrete actions
-        // throw new io.cucumber.java.PendingException();
-        assertFalse("Login should fail", obj.f);
+        assertFalse("Login should fail", obj.validation);
     }
 
     @When("set valid username {string} and invalid pass {string}")
@@ -79,32 +63,15 @@ public class loginstep {
 
     @When("set valid username {string} and  pass {string}")
     public void setValidUsernameAndPass(String user_name, String pass) {
-        forget = false;
-        for (User u: obj.up) {
-            if (user_name.equals(u.getUser_name()) && pass.equals("Forget")) {
-                forget = true;
-                enteredUsername = user_name;
-                break;
-            }
-        }
+        obj.validUserPass(user_name,pass);
+        forget=obj.forget;
+        enteredUsername= obj.enteredUsername;
     }
 
     @Then("take new pass {string}")
     public void takeNewPass(String newPass) {
-        for (User user : obj.up) {
-            if (user.getUser_name().equals(enteredUsername)) {
-                user.setPass(newPass);
-            }
-        }
-        boolean passwordUpdated = false;
-        for (User user : obj.up) {
-            if (user.getUser_name().equals(enteredUsername) && user.getPass().equals(newPass)) {
-                passwordUpdated = true;
-                break;
-            }
-        }
-
-        assertTrue("New password should be updated", passwordUpdated);
+        obj.takePass(newPass);
+        assertTrue("New password should be updated", obj.passwordUpdated);
     }
 
     @And("i don't have an account")
@@ -121,15 +88,8 @@ public class loginstep {
 
     @Then("create succeed")
     public void createSucceed() {
-        boolean userCreated = false;
-        for (User user : obj.up) {
-            if (user.getUser_name().equals(enteredUsername) && user.getPass().equals(enteredPassword)) {
-                userCreated = true;
-                break;
-            }
-        }
-
-        assertTrue("User creation should succeed", userCreated);
+        obj.createAcc(enteredUsername,enteredPassword);
+        assertTrue("User creation should succeed", obj.userCreated);
     }
 }
 
