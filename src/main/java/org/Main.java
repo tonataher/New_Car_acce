75f73301e9e03b87a6522ed38af406568f15f23a
MaiSalameh <145279461+MaiSalameh@users.noreply.github.com>
2023-12-20 20:07:28 +0200

    Update Main.java

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -12,7 +12,7 @@ public class Main {
     static boolean cm=true;
     static boolean cm2=true;
     static public String genera;
-    static public String Description = " || Description about it:- ";
+    static final public String Description = " || Description about it:- ";
     public static int exf(String ku,String p)
     {
         int l=0;
