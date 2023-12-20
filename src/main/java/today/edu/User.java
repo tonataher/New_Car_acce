a6618d73ddbfbd7d8427a62e4cbede8ed44647e9
MaiSalameh <145279461+MaiSalameh@users.noreply.github.com>
2023-12-20 19:40:55 +0200

    Update User.java

--- src/main/java/today/edu/User.java
+++ src/main/java/today/edu/User.java
@@ -14,9 +14,6 @@ public class User
     public String getUser_name() {
         return user_name;
     }
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
     public String getPass() {
         return pass;
     }
